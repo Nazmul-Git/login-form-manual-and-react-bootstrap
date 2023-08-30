88e7af7a1a2ac917acfffe0a1fe47e2c54bd3fa9
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-08-31 03:27:55 +0600

    User register pass authenticated with successfull register and error message.

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import app from '../../Firebase/firebase.config';
+
+const auth= getAuth(app);
 
 const Register = () => {
-    const [email, setEmail]=useState('');
+    const [error, setError]= useState('');
+    const [successful, setSuccessful]=useState('');
 
     const emailOnChange=(event)=>{
         console.log(event.target.value);
-        setEmail(event.target.value);
+        // setEmail(event.target.value);
     }
 
     const passOnBlur=(event)=>{
@@ -15,18 +20,51 @@ const Register = () => {
 
     const registerSubmit=(event)=>{
         event.preventDefault();
+        setSuccessful('');
+        setError('');
         const email=event.target.email.value;
         const pass=event.target.password.value;
-        console.log(email, pass);
+        // console.log(email, pass);
+
+        //validation pass
+        if(!/(?=.*[A-Z])/.test(pass)){
+            setError('Please..at list one uppercase latter !');
+            return;
+        }
+        else if(!/(?=.{8,})/.test(pass)){
+            setError('Password should be 8 character or long !');
+            return;
+        }
+        else if(!/(?=.*[!@#$&*])/.test(pass)){
+            setError('Please add a special character.');
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth,email,pass)
+        .then(result=>{
+            const loggedUser=result.user;
+            console.log(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccessful('Created successfully !!');
+
+        })
+        .catch(error=>{
+            console.error(error.message);
+            setError(error.message);
+            setSuccessful('');
+        })
     }
     return (
-        <div className=' text-center'>
+        <div className=' '>
             <h2>Register Here..</h2>
             <form onSubmit={registerSubmit} className='grid grid-cols-1 h-20 w-72 m-48 gap-6'>
-                <input onChange={emailOnChange} type="email" name="email" id="email" placeholder='Your email ' />
-                <input onBlur={passOnBlur} type="password" name="password" id="password" placeholder='Your password' />
-                <input type="submit" value="Register" />
+                <input onChange={emailOnChange} className='p-2' required   type="email" name="email" id="email" placeholder='Your email ' />
+                <input onBlur={passOnBlur} className='p-2' required    type="password" name="password" id="password" placeholder='Your password' />
+                <input className=' bg-blue-600 p-2 rounded-md text-2xl text-white' type="submit" value="Register" />
             </form>
+            <p className=' text-danger'> {error}</p>
+            <p className=' text-success'> {successful}</p>
         </div>
     );
 };
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUrdERJQ8pjJrI_QtjOqkhs788BnWfTy4",
+  authDomain: "email-pass-authenticatio-6592a.firebaseapp.com",
+  projectId: "email-pass-authenticatio-6592a",
+  storageBucket: "email-pass-authenticatio-6592a.appspot.com",
+  messagingSenderId: "306801931253",
+  appId: "1:306801931253:web:c601874908dd738d78e6bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
